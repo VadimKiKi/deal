4adfd428d3e64d28ed84943ad9a3fbfef3421fa6
Vadim Taratonov <taratonovv8@gmail.com>
2023-08-27 22:52:00 +0300

    Add new service and controller

---
+++ src/main/java/ru/taratonov/deal/controller/DocumentKafkaController.java
@@ -0,0 +1,93 @@
+package ru.taratonov.deal.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.taratonov.deal.dto.ErrorDTO;
+import ru.taratonov.deal.enums.Theme;
+import ru.taratonov.deal.service.DocumentKafkaService;
+
+@RestController
+@RequestMapping("/deal/document")
+@RequiredArgsConstructor
+@Tag(name = "Document Controller", description = "Provides the ability to manage documents")
+public class DocumentKafkaController {
+
+    private final DocumentKafkaService documentKafkaService;
+
+    @PostMapping("/{applicationId}/send")
+    @Operation(summary = "Send documents", description = "Allows to send documents to client email")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Document has been sent!",
+            content = @Content(mediaType = "application/json"))
+    @ApiResponse(
+            responseCode = "404",
+            description = "Application not found",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorDTO.class)))
+    public void sendDocument(@Parameter(description = "Id of the application", required = true)
+                             @PathVariable("applicationId") Long id) {
+        documentKafkaService.sendDocuments(id, Theme.SEND_DOCUMENTS);
+    }
+
+    @PostMapping("/{applicationId}/sign")
+    @Operation(summary = "Request to sign documents", description = "Allows to send special code for singing documents")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Code has been sent!",
+            content = @Content(mediaType = "application/json"))
+    @ApiResponse(
+            responseCode = "404",
+            description = "Application not found",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorDTO.class)))
+    public void requestSignDocument(@Parameter(description = "Id of the application", required = true)
+                                    @PathVariable("applicationId") Long id) {
+        documentKafkaService.requestSignDocument(id, Theme.SEND_SES);
+    }
+
+    @PostMapping("/{applicationId}/code")
+    @Operation(summary = "Sign document with code", description = "Allows check special code and send email with successful loan request")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Credit issued!",
+            content = @Content(mediaType = "application/json"))
+    @ApiResponse(
+            responseCode = "404",
+            description = "Application not found",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorDTO.class)))
+    public void signDocumentWithCode(@Parameter(description = "Id of the application", required = true)
+                                     @PathVariable("applicationId") Long id,
+                                     @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                                             description = "SES code")
+                                     @RequestBody Integer sesCode) {
+        documentKafkaService.signDocument(id, sesCode, Theme.CREDIT_ISSUED);
+    }
+
+    @PostMapping("/{applicationId}/deny")
+    @Operation(summary = "Deny application", description = "Allows to deny the application")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Application denied!",
+            content = @Content(mediaType = "application/json"))
+    @ApiResponse(
+            responseCode = "404",
+            description = "Application not found",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorDTO.class)))
+    public void denyApplication(@Parameter(description = "Id of the application", required = true)
+                                @PathVariable("applicationId") Long id) {
+        documentKafkaService.denyApplication(id);
+    }
+}
